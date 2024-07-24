c0bf5939209cb0e40a1b6c8d8a002347f04f5ca2
kasinathanb-aot <kasinathan@aot-technologies.com>
2024-07-24 04:53:07 -0700

    Answer submission completed

--- src/components/quizcard/QuizCard.jsx
+++ src/components/quizcard/QuizCard.jsx
@@ -1,20 +1,41 @@
 import React, { useState } from "react";
 import { quiz } from "../../utils/dataSet";
 import "./quizcard.scss";
+
 function QuizCard({ level }) {
   const formattedLevel = level.toLowerCase();
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [score, setScore] = useState(0);
+  const [isDisabled, setDisabled] = useState(false);
   const quizData = quiz.levels[formattedLevel];
-  // handle next
-  const handleNext = () => {
-    if (currentIndex != quizData.totalQuestions - 1) {
-      setCurrentIndex(currentIndex + 1);
+  const defaultScore = quizData.perQuestionScore;
+
+  const handleAnswer = (choice) => {
+    setSelectedAnswer(choice);
+    setDisabled(true);
+    const isCorrect = choice === quizData.questions[currentIndex].correctAnswer;
+
+    const newUserAnswers = [...userAnswers];
+    newUserAnswers[currentIndex] = {
+      chosenAnswer: choice,
+    };
+    setUserAnswers(newUserAnswers);
+    console.log(userAnswers);
+    if (isCorrect) {
+      setScore((prevScore) => prevScore + defaultScore);
+    } else {
+      console.log("Wrong answer");
     }
+    setTimeout(handleNext, 1000);
   };
-  // handle previous
-  const handlePrev = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
+
+  const handleNext = () => {
+    if (currentIndex !== quizData.totalQuestions - 1) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+      setDisabled(false);
+      setSelectedAnswer(userAnswers[currentIndex + 1]?.chosenAnswer || "");
     }
   };
 
@@ -34,13 +55,32 @@ function QuizCard({ level }) {
             <p>{quizData.questions[currentIndex].question}</p>
             <ul>
               {quizData.questions[currentIndex].choices.map((choice, i) => (
-                <li key={i}>{choice}</li>
+                <li
+                  key={i}
+                  onClick={() => !isDisabled && handleAnswer(choice)}
+                  style={{
+                    background:
+                      selectedAnswer === choice
+                        ? selectedAnswer ===
+                          quizData.questions[currentIndex].correctAnswer
+                          ? "#0BDA51"
+                          : "#FF5733"
+                        : "",
+                    pointerEvents: isDisabled ? "none" : "auto",
+                  }}
+                >
+                  {choice}
+                </li>
               ))}
             </ul>
           </div>
           <div className="btn-container">
-            <button onClick={() => handlePrev()}>Prev</button>
-            <button onClick={() => handleNext()}>Next</button>
+            <button
+              onClick={handleNext}
+              disabled={currentIndex === quizData.totalQuestions - 1}
+            >
+              Next
+            </button>
           </div>
         </div>
       ) : (
